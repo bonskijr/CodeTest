2b889d47821f46b6f9b70b79ce4fae41919d610a
bonskijr <jedi_knyt23@yahoo.com>
2013-11-29 00:08:33 +0800

    Revert "Added FancyConsole"
    This reverts commit 1a44235a5720b41a7d9132502d290e83f1972e72.

--- Harness/Console/Program.cs
+++ Harness/Console/Program.cs
@@ -9,15 +9,11 @@ namespace CodeTest.ConsoleApplication
     {
         static void Main(string[] args)
         {
-
-
-   
-
             string inputString;
             string subText;
             TextMatcher textMatcher;
             DisplayOutput displayOut;
-           // WriteCharacterStrings();
+
             Console.WriteLine("-----------------------------------------");
             Console.WriteLine("** TextMatcher test harness **");
             Console.WriteLine("-----------------------------------------");
@@ -31,7 +27,7 @@ namespace CodeTest.ConsoleApplication
                 subText = Console.ReadLine();
 
                 textMatcher = new TextMatcher(inputString, subText);
-                displayOut = new DisplayOutput(new FancyConsoleWriter(inputString, subText));
+                displayOut = new DisplayOutput(new ConsoleWriter());
                 displayOut.Display(textMatcher.GetMatchingPositions());
 
                 Console.WriteLine("\nPress <ESC> to quit, any key to resume");
@@ -40,20 +36,6 @@ namespace CodeTest.ConsoleApplication
 
             Console.WriteLine("Press the ENTER key to finish...");
             Console.ReadLine();
-        }
-        private static void WriteCharacterStrings()
-        {
-            var defColor = Console.BackgroundColor;
-
-            Console.Write("The");
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.Write("quick");
-            Console.BackgroundColor = defColor;
-            Console.Write("brown");
-            Console.BackgroundColor = ConsoleColor.Yellow;
-            Console.Write("fox");
-            Console.BackgroundColor = defColor;
-
         }
     }
 }
